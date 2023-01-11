1eaf49574d7d99a5a6b14ea3679061a577e5ad37
Ravi Chandola <ravi.chandola@litera.com>
2023-01-11 21:15:47 +0530

    Hardcoded Data

--- App.js
+++ App.js
@@ -1,32 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import logo from "./images/icon.png";
-import home from "./images/home.png";
-
-
-
-/* const SearchComponent = () => {
-  return (
-    <input
-      className="search"
-      key="search-bar"
-      placeholder={"I think you travel to search and you come back home to find yourself there"}
-    />
-  );
-};
-*/
-
-/*const NavigationBar = () =>{
-  return(
-    <div className="NavigationBar">
-    <img src={logo} alt="logo" width="40px" height="40px" />
-    <SearchComponent />
-    {Title()}
-    <img src={home} alt="logo" width="40px" height="40px" />
-  </div>
-  )
-}
-*/
 
 const Title = () =>(
   <h1 id="title" key="h2">
@@ -45,43 +18,43 @@ const HeaderComponent = () => {
         <li>Contact</li>
         <li>Cart</li>
       </ul>
-
-
     </div>
-    
-
    </div>
   )
 };
 
+const RestrauntCard = () =>{
+  return(
+    <div className="card">
+      <img src = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/egl4xnbo92lybrlw2hg2"/>
+      <h2>Burger King</h2>
+      <h3>Burgers, American</h3>
+      <h4>4.2 Stars</h4>
+    </div>
+  )
+}
 
+const BodyComponent = () =>{
+  return(
+  <div>
+   <RestrauntCard/>
+   </div>
+  )
+}
+
+const FooterComponent = () =>{
+  return <h3>Footer Components</h3>
+}
 
 const AppLayout = () => {
   return(
-      {/**
-        Header Component
-          -Logo
-          -List Items (Right Side)
-          - Cart
-        Body
-          - Search Bar
-          - Restaurant List
-          - Restaurant Cards
-            - Image
-            - Name
-            - Rating
-            - Cuisines
-        Footer
-          - Links
-          - Copyright
-      */
-      
-      }
+      <>
+      <HeaderComponent/>
+      <BodyComponent/>
+      <FooterComponent/>
+      </>
     )
-
-
 }
 
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  // passing react element inside root
-  root.render(<HeaderComponent/>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
