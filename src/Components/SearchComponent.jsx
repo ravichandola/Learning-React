61cffb193df856f50c6a61f9b01c4726c79b2517
Ravi Chandola <ravi.chandola@litera.com>
2023-01-16 21:53:15 +0530

    Added useState()

--- src/Components/SearchComponent.jsx
+++ src/Components/SearchComponent.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react"; //named export
+
 const SearchComponent = () => {
+  // const searchText = "KFC";
+  // searchText is the local state variable
+  const [searchText, setSearchText] = useState();
   return (
     <div className="search-container">
       <input
         type="text"
         className="Search-input"
         placeholder="Search"
-        value=""
+        value={searchText}
+        onChange={(e) => {
+          setSearchText(e.target.value);
+        }}
       />
       <button className="search-btn">Search</button>
     </div>
